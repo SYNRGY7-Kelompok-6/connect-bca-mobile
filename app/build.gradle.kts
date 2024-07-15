01d6659d4334f33c6e9d340703ad7a0feadc5298
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-07-15 11:11:48 +0700

    initial slicing of login page (light mode): added layout, logo, welcome text, password field, and login button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
