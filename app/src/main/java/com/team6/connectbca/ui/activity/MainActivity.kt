dbf93f69e86f6b3aafbb5359c73de1ee24cdfbc0
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-07-20 22:18:00 +0700

    Update: bottom sheets behavior to make it more responsive to different devices

--- app/src/main/java/com/team6/connectbca/ui/activity/MainActivity.kt
+++ app/src/main/java/com/team6/connectbca/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.team6.connectbca.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.tabs.TabLayoutMediator
 import com.team6.connectbca.databinding.ActivityMainBinding
 import com.team6.connectbca.ui.fragment.TabPagerAdapter
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         setupTabLayout()
+        setupBottomSheet()
     }
 
     private fun setupTabLayout() {
@@ -37,5 +39,26 @@ class MainActivity : AppCompatActivity() {
                 else -> null
             }
         }.attach()
+    }
+
+    private fun setupBottomSheet() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+
+        val screenHeight = resources.displayMetrics.heightPixels
+
+        val peekHeight = when {
+            screenHeight <= 480 -> screenHeight * 0.15
+            screenHeight <= 800 -> screenHeight * 0.2
+            screenHeight <= 1024 -> screenHeight * 0.25
+            screenHeight <= 1280 -> screenHeight * 0.3
+            screenHeight <= 1440 -> screenHeight * 0.35
+            screenHeight <= 1920 -> screenHeight * 0.4
+            else -> screenHeight * 0.45
+        }
+
+        bottomSheetBehavior.peekHeight = peekHeight.toInt()
+
+        val maxHeight = (screenHeight * 1.0).toInt()
+        bottomSheetBehavior.maxHeight = maxHeight
     }
 }
