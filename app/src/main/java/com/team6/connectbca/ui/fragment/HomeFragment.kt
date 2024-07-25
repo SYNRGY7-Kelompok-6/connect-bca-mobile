6711c1f802db15d27359a4c34fce1545786cabb2
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-07-25 12:33:26 +0700

    Add: copy to clipboard feature to balance card at home fragment

--- app/src/main/java/com/team6/connectbca/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/team6/connectbca/ui/fragment/HomeFragment.kt
@@ -1,5 +1,49 @@
 package com.team6.connectbca.ui.fragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.team6.connectbca.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment()
+class HomeFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnIconCopy.setOnClickListener {
+            val accountNumber = binding.tvAccountNumber.text.toString()
+            copyToClipboard(accountNumber)
+        }
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboardManager = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Copied Text", text)
+        clipboardManager.setPrimaryClip(clip)
+        showToast("Text copied to clipboard")
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+}
