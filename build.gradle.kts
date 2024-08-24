6f446a8c914125a6412cab9d29c52a87941717be
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-08-25 00:23:28 +0700

    Update CI/CD script and data module gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath("com.android.tools.build:gradle:8.0.2")
     }
 }
 
