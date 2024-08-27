4462b3dc473110abf7d6d14dab91aa2cdce4924e
MZannn <mfauzan1098@gmail.com>
2024-08-27 10:40:22 +0700

    pin gravity

--- app/src/main/java/com/team6/connectbca/ui/fragment/pin/PinFragment.kt
+++ app/src/main/java/com/team6/connectbca/ui/fragment/pin/PinFragment.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -188,7 +189,7 @@ class PinFragment : Fragment() {
         for (i in 0 until 6) {
             if (i < length) {
                 pinDigits[i].text = "*"
-                pinDigits[i].gravity = View.TEXT_ALIGNMENT_CENTER
+                pinDigits[i].gravity = Gravity.CENTER
                 pinDigits[i].setBackgroundResource(R.drawable.filled_circle_background)
             } else {
                 pinDigits[i].text = ""
