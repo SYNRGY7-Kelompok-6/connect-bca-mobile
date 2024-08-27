a6944d0356462db7e0ccf5c3997eeb989765cd09
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-08-27 20:39:24 +0700

    Update: versioning to dynamically update based on latest GitHub releases

--- bump-version.sh
+++ bump-version.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Fetch the latest tag from GitHub
+latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+# Check if latest_tag is empty or null
+if [ -z "$latest_tag" ]; then
+  echo "No tags found. Setting default version 0.0.0."
+  latest_tag="v0.0.0"
+fi
+
+# Remove the 'v' prefix if it exists
+current_version=${latest_tag#v}
+
+# Get the current versionCode from the root build.gradle.kts
+current_version_code=$(grep "versionCode =" build.gradle.kts | awk '{ print $3 }')
+
+# Check if current_version_code is empty or null
+if [ -z "$current_version_code" ]; then
+  echo "Error: current_version_code is empty or null. Setting default versionCode 1."
+  current_version_code=1
+fi
+
 # Check if there are enough commits to perform the diff
 if git rev-parse HEAD~1 >/dev/null 2>&1; then
   changed_files=$(git diff --name-only HEAD~1 HEAD | wc -l)
@@ -8,15 +29,11 @@ else
   changed_files=0
 fi
 
-# Get the current versionName and versionCode from the root build.gradle.kts
-current_version=$(grep "versionName =" build.gradle.kts | awk '{ print $3 }' | tr -d '"')
-current_version_code=$(grep "versionCode =" build.gradle.kts | awk '{ print $3 }')
-
 # Split the current versionName into its components
 IFS='.' read -r -a version_parts <<< "$current_version"
-MAJOR=${version_parts[0]}
-MINOR=${version_parts[1]}
-PATCH=${version_parts[2]}
+MAJOR=${version_parts[0]:-0}
+MINOR=${version_parts[1]:-0}
+PATCH=${version_parts[2]:-0}
 
 # Determine the increment based on the number of changed files
 if [ "$changed_files" -le 5 ]; then
